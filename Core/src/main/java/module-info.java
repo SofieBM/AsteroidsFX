cab658630ebc17235a323cfa5ecc1094338fe435
sofie <sofim23@student.sdu.dk>
2025-05-21 01:15:22 +0200

    Lab(java) fra anden forlæsning + enemy da der var en fejl med modulet som blev fikset

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,16 +1,23 @@
 module Core {
     requires Common;
-
     requires CommonBullet;    
     requires javafx.graphics;   
     requires spring.context;
     requires spring.core;
     requires spring.beans;
+
+    requires Player; // Kræv Player-modulet
+    requires Asteroid; // Kræv Asteroid-modulet
+    requires Bullet; // Kræv Bullet-modulet
+    requires Collision;
+    requires Enemy;
+
     exports dk.sdu.mmmi.cbse.main;
     opens dk.sdu.mmmi.cbse.main to javafx.graphics,spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+    uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
 }
 
 
---
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/EnemyPlugin.java
@@ -0,0 +1,49 @@
+package dk.sdu.mmmi.cbse;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import java.util.Random;
+
+
+public class EnemyPlugin implements IGamePluginService
+
+{
+    private Entity enemy;
+    private Random rnd = new Random();
+
+    public EnemyPlugin()
+    {
+    }
+
+    @Override
+    public void start(GameData gameData, World world)
+    {
+    // Opreter et Enemy spaceship, når spillet starter
+        enemy = createEnemyShip(gameData);
+        world.addEntity(enemy);
+    }
+
+
+    private Entity createEnemyShip(GameData gameData)
+    {
+        Entity enemyShip = new Entity();
+
+        enemyShip.setPolygonCoordinates(-7,-7,10,0,-7,7);
+
+        // det her gør at fjenden bliver tilføjet et tilfældigt sted på skærmen
+        enemyShip.setX(rnd.nextInt(gameData.getDisplayWidth()));
+        enemyShip.setY(rnd.nextInt(gameData.getDisplayHeight()));
+        enemyShip.setRadius(7);
+        return enemyShip;
+    }
+
+
+    @Override
+    public void stop(GameData gameData, World world)
+    {
+        // det her gør så enemy spaceshipet bliver fjernet når plugin'et stopper
+        world.removeEntity(enemy);
+    }
+}
