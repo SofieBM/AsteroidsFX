0cce2a9a22f4c510a619a867ea46f1b746563fbd
sofie <sofim23@student.sdu.dk>
2025-05-21 16:10:02 +0200

    små ændringer og kommentarer

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
@@ -4,6 +4,7 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 public interface IEntityProcessingService {
+    //Interface-niveau: Beskriv generelt, hvad dette interface repræsenterer. (F.eks., "Interface for services, der behandler spilenheders logik hvert spil-tick.")
 
     /**
      *
@@ -14,4 +15,9 @@ public interface IEntityProcessingService {
      * @throws
      */
     void process(GameData gameData, World world);
+    //Formål: Beskriv, hvad denne metode gør (f.eks., "Opdaterer tilstand for spilenheder (f.eks. position, hastighed, orientering) baseret på spillets logik og input.").
+    //@param gameData: Samme som ovenfor.
+    //@param world: Samme som ovenfor.
+    //Pre-conditions: (F.eks., "gameData og world må ikke være null. world skal indeholde de entiteter, der skal behandles af denne service.")
+    //Post-conditions: (F.eks., "Entiteternes tilstand er opdateret, og eventuelle nye entiteter (f.eks. skud) er tilføjet, mens ødelagte entiteter (f.eks. fjerne skud) er markeret til fjernelse.")
 }
--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
@@ -7,7 +7,14 @@ import dk.sdu.mmmi.cbse.common.data.World;
  *
  * @author jcs
  */
-public interface IPostEntityProcessingService {
+public interface IPostEntityProcessingService
+{
+    //Interface-niveau: Beskriv generelt, hvad dette interface repræsenterer. (F.eks., "Interface for services, der udfører handlinger efter alle entiteter er blevet behandlet, typisk til kollisionsdetektion eller oprydning.")
 
     void process(GameData gameData, World world);
+    //Formål: Beskriv, hvad denne metode gør (f.eks., "Håndterer interaktioner mellem spilenheder, såsom kollisioner, og opdaterer spilverdenen baseret på disse interaktioner.").
+    //@param gameData: Samme som ovenfor.
+    //@param world: Samme som ovenfor.
+    //Pre-conditions: (F.eks., "gameData og world må ikke være null. Entiteternes positioner skal være opdateret af IEntityProcessingService services.")
+    //Post-conditions: (F.eks., "Kolliderende entiteter er blevet behandlet (f.eks. ødelagt, delt, tildelt skade), og world er opdateret i overensstemmelse hermed.")
 }
